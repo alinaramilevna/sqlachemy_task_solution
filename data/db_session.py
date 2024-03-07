0b734d4933060ec4159fc53a930a2155f316f02c
alinaKush <alina.kush2009@gmail.com>
2024-03-07 23:13:51 +0500

    форма регистрации работает!!!

--- data/db_session.py
+++ data/db_session.py
@@ -19,7 +19,7 @@ def global_init(db_file):
     conn_str = f'sqlite:///{db_file.strip()}?check_same_thread=False'
     print(f"Подключение к базе данных по адресу {conn_str}")
 
-    engine = sa.create_engine(conn_str, echo=True)  # SQl - лог в консоль
+    engine = sa.create_engine(conn_str)  # SQl - лог в консоль echo=True (меня бесило, я отключила)
     __factory = orm.sessionmaker(bind=engine)
 
     from . import __all_models
